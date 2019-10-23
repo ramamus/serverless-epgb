847fccf8a401f2a734151db64dfdf805fc33f6ec
Satish Ramamurthi <satish.ramamurthi@target.com>
2019-10-23 11:58:15 -0500

    initial serverless setttings

---
+++ handler.js
@@ -0,0 +1,106 @@
+const db = require('./db_connect');
+
+module.exports.getAllPlayers = (event, context, callback) => {
+  context.callbackWaitsForEmptyEventLoop = false;
+  db.getAll('player')
+    .then(res => {
+      callback(null, {
+        statusCode: 200,
+        headers: {
+          'Access-Control-Allow-Origin': '*'
+        },
+        body: JSON.stringify(res)
+      });
+    })
+    .catch(e => {
+      console.log(e);
+      callback(null, {
+        statusCode: e.statusCode || 500,
+        body: 'Error: Could not find Players: ' + e
+      });
+    });
+};
+
+module.exports.createPlayer = (event, context, callback) => {
+  context.callbackWaitsForEmptyEventLoop = false;
+  const data = JSON.parse(event.body);
+  db.insert('player', data)
+    .then(res => {
+      callback(null, {
+        statusCode: 200,
+        headers: {
+          'Access-Control-Allow-Origin': '*'
+        },
+        body: 'Player Created! id: ' + res
+      });
+    })
+    .catch(e => {
+      callback(null, {
+        statusCode: e.statusCode || 500,
+        body: 'Could not create Player ' + e
+      });
+    });
+};
+
+module.exports.deletePlayer = (event, context, callback) => {
+  context.callbackWaitsForEmptyEventLoop = false;
+
+  db.deleteById('player', event.pathParameters.id)
+    .then(res => {
+      callback(null, {
+        statusCode: 200,
+        headers: {
+          'Access-Control-Allow-Origin': '*'
+        },
+        body: 'Player Deleted!'
+      });
+    })
+    .catch(e => {
+      callback(null, {
+        statusCode: e.statusCode || 500,
+        body: 'Could not delete Player ' + e
+      });
+    });
+};
+
+module.exports.updatePlayer = (event, context, callback) => {
+  context.callbackWaitsForEmptyEventLoop = false;
+
+  const data = JSON.parse(event.body);
+  db.updateById('player', event.pathParameters.id, data)
+    .then(res => {
+      callback(null, {
+        statusCode: 200,
+        headers: {
+          'Access-Control-Allow-Origin': '*'
+        },
+        body: 'Player Updated!' + res
+      });
+    })
+    .catch(e => {
+      callback(null, {
+        statusCode: e.statusCode || 500,
+        body: 'Could not update Player ' + e
+      });
+    });
+};
+
+module.exports.getPlayer = (event, context, callback) => {
+  context.callbackWaitsForEmptyEventLoop = false;
+  db.getById('player', event.pathParameters.id)
+    .then(res => {
+      callback(null, {
+        statusCode: 200,
+        headers: {
+          'Access-Control-Allow-Origin': '*'
+        },
+        body: JSON.stringify(res)
+      });
+    })
+    .catch(e => {
+      callback(null, {
+        statusCode: e.statusCode || 500,
+        body: 'Could not find Player: ' + e
+      });
+    });
+};
